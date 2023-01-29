6b23326ffecf432383afb5cfaf4fa71d221878b9
Barmendu Chakraborty <officialbarmenduchakraborty@gmail.com>
2023-01-30 04:54:49 +0530

    Added cam add and more icons

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import Tom from "../img/Tom.jpeg"
 
 const Chats = () => {
   return (
-    <div>Chats</div>
+    <div className="chats">
+       <div className="userChat">
+        <img src={Tom} alt="" />
+        <div className="userChatInfo">
+          <span>Tom</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img src={Tom} alt="" />
+        <div className="userChatInfo">
+          <span>Tom</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img src={Tom} alt="" />
+        <div className="userChatInfo">
+          <span>Tom</span>
+          <p>Hello</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
