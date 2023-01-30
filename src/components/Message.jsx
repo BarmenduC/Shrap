f5dfd9e6cae7c1626199401878240bb21084bdb2
Barmendu Chakraborty <officialbarmenduchakraborty@gmail.com>
2023-01-31 04:46:16 +0530

    Firebase initialised

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Tom from "../img/Tom.jpeg"
 
 const Message = () => {
   return (
-    <div>Message</div>
+    <div className="message owner">
+      <div className="messageInfo">
+        <img src={Tom} alt="" />
+        <span>Just now</span>
+      </div>
+      <div className="messageContent">
+        <p>Hello</p>
+        <img src={Tom} alt="" />
+      </div>
+    </div>
   )
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDG2JXxoADrgfQTuZdHKGfeZJn4uSP9dFs",
+  authDomain: "shrap-75daa.firebaseapp.com",
+  projectId: "shrap-75daa",
+  storageBucket: "shrap-75daa.appspot.com",
+  messagingSenderId: "828440793543",
+  appId: "1:828440793543:web:bcb1dff2c4170952d1c9e2"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth()
